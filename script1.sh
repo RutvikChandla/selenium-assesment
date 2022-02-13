db83bc5aa6c93b41d5de805d7d795caa65bdd840
RutvikChandla <rutvik.c@browserstack.com>
2022-02-13 15:10:03 +0530

    phase 2

--- script1.sh
+++ script1.sh
@@ -4,12 +4,43 @@ echo_notice() {
     NC='\033[0m' # No Color
     printf "\n${GREEN}${msg}${NC}"
 }
+bst_flag=0
 
-echo_notice "Running session on selenium.." 
-ROOT=http://192.168.0.101:4444
+optspec=":-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                browserstack)
+                    bst_flag=1
+                    ;;
+                *)
+                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                        echo "Unknown option --${OPTARG}" >&2
+                    fi
+                    ;;
+            esac;;
+    esac
+done
+
+
+if [[ $bst_flag -eq 1 ]]
+then
+    echo_notice "Running session orowserstack" >&2
+    ROOT=https://rutvikchandla_2MEern:QRRxxzZRAZxqQFLsocrk@hub-cloud.browserstack.com/wd/hub
+else
+    echo_notice "Running session on selenium.." 
+    ROOT=http://192.168.0.101:4444
+fi
 
 main() {
-    SESSION_ID=$(get_session_id)
+    if [[ $bst_flag -eq 1 ]]
+    then
+        SESSION_ID=$(get_session_id_bst)
+    else
+        SESSION_ID=$(get_session_id)
+    fi
+
     navigate_to 'https://google.co.in'
     searchBox=$(find_element 'name' 'q')
     send_keys $searchBox "rutvik\n"
@@ -22,6 +53,23 @@ get_session_id() {
         -d '{
             "desiredCapabilities": {
                 "browserName": "chrome"
+            }
+        }' \
+        ${ROOT}/session | jq -r '.sessionId'
+}
+
+get_session_id_bst() {
+    curl -s -X POST -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-CH: RTT' -H 'Keep-Alive: true'\
+        -d '{
+            "desiredCapabilities": {
+                "browserName": "Chrome",
+                "os": "OS X",
+                "os_version": "Sierra",
+                "browser_version": "65.0",
+                "build": "selenium-bash-assesment",
+                "acceptSslCerts": true,
+                "browserstack.debug": true,
+                "browserstack.console": "verbose"
             }
         }' \
         ${ROOT}/session | jq -r '.sessionId'
